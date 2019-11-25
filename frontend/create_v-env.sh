713cef7e9370c9215e699d874b6e740090b7573f
simmarum <mateusz0831@gmail.com>
2019-11-25 12:37:21 +0100

    Split project for backend and frontend, add n-env for node

---
+++ frontend/create_v-env.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Simple bash script to recreate env for this repository
+# Activate env by typing
+#
+# source ./v-env/bin/activate
+#
+
+set -e
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+rm -rf $SCRIPTPATH/v-env
+rm -rf $SCRIPTPATH/n-env
+
+python3 -m venv $SCRIPTPATH/v-env
+. $SCRIPTPATH/v-env/bin/activate
+
+pip3 install --compile \
+nodeenv==1.3.3
+
+. $SCRIPTPATH/v-env/bin/activate
+
+nodeenv $SCRIPTPATH/n-env
