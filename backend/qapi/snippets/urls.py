5cd9bbd81dddbfb96c15168f479e1c846e4ab39a
simmarum <mateusz0831@gmail.com>
2019-11-26 18:49:38 +0100

    Add main page for API site

--- backend/qapi/snippets/urls.py
+++ backend/qapi/snippets/urls.py
@@ -5,10 +5,12 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
-    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/', views.SnippetList.as_view(), name='snippets-list'),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
-    path('users/', views.UserList.as_view()),
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()),
+    path('users/', views.UserList.as_view(), name='users-list'),
     path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('', views.api_root),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
