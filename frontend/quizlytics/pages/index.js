96871c0e1fa277f0b1ca1bb6402cba981147cf3d
simmarum <mateusz0831@gmail.com>
2019-11-26 12:32:27 +0100

    Add common components to app

--- frontend/quizlytics/pages/index.js
+++ frontend/quizlytics/pages/index.js
@@ -1,13 +1,10 @@
-// This is the Link API
-import Link from 'next/link';
+import Header from '../comps/Header';
 
-const Index = () => (
-    <div>
-        <Link href="/about">
-            <a title="About Page">About Page</a>
-        </Link>
-        <p>Hello Next.js</p>
-    </div>
-);
-
-export default Index;
+export default function Index() {
+    return (
+        <div>
+            <Header />
+            <p>Hello Next.js</p>
+        </div>
+    );
+}
