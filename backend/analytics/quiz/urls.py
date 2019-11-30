a62e856df81c225427b0c57e47b74acbaa6be30a
simmarum <mateusz0831@gmail.com>
2019-11-30 15:35:01 +0100

    Add cities to API urls

--- backend/analytics/quiz/urls.py
+++ backend/analytics/quiz/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from quiz.views import UserViewSet
+from quiz.views import UserViewSet, CityViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'cities', CityViewSet)
 
 urlpatterns = [
     url(r'', include(router.urls)),
