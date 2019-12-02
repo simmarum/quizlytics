3e2081997815983477a09334bee7ca002783963e
simmarum <mateusz0831@gmail.com>
2019-12-02 21:17:18 +0100

    Get user data on profile site

--- frontend/quizlytics/pages/profile.js
+++ frontend/quizlytics/pages/profile.js
@@ -5,29 +5,12 @@ import { auth, get_auth_header } from '../utils/auth'
 import { api_path } from '../utils/api_path'
 
 const Profile = props => {
-  console.log("!!!", props)
+  const user_profile = props.results[0]
+  console.log("#@", user_profile)
   return (
     <div>
       <h1>Profile</h1>
 
-      <style jsx>{`
-        img {
-          max-width: 200px;
-          border-radius: 0.5rem;
-        }
-        h1 {
-          margin-bottom: 0;
-        }
-        .lead {
-          margin-top: 0;
-          font-size: 1.5rem;
-          font-weight: 300;
-          color: #666;
-        }
-        p {
-          color: #6a737d;
-        }
-      `}</style>
     </div>
   )
 }
@@ -40,24 +23,17 @@ Profile.getInitialProps = async ctx => {
   const url = api_path.users
   try {
     const response = await fetch(url, {
-      // credentials: 'always',
       headers: get_auth_header(token)
     })
     console.log("$", response)
     if (response.ok) {
       return await response.json()
     } else {
-      console.log("E1")
       console.log(response)
-      // https://github.com/developit/unfetch#caveats
-      // return redirectOnError()
     }
   } catch (error) {
     // Implementation or Network error
-
-    console.log("E2")
     console.log(error)
-    // return redirectOnError()
   }
 }
 
