abcf66840cb3ac9a12483b6622bd2f1a752b1e67
simmarum <mateusz0831@gmail.com>
2019-12-02 21:43:56 +0100

    Use global style in Layout

--- frontend/quizlytics/components/MyLayout.js
+++ frontend/quizlytics/components/MyLayout.js
@@ -7,13 +7,43 @@ const layoutStyle = {
 };
 
 const Layout = props => {
-    console.log("^", props)
     let header_pros = {}
     header_pros.is_logged = props.is_logged
     return (
-        <div style={layoutStyle}>
+        <div>
             <Header {...header_pros} />
             {props.children}
+
+            <style jsx global>{`
+        .login {
+          max-width: 340px;
+          margin: 0 auto;
+          padding: 1rem;
+          border: 1px solid #ccc;
+          border-radius: 4px;
+        }
+        form {
+          display: flex;
+          flex-flow: column;
+        }
+        label {
+          font-weight: 600;
+        }
+        input {
+          padding: 8px;
+          margin: 0.3rem 0 1rem;
+          border: 1px solid #ccc;
+          border-radius: 4px;
+        }
+        .error {
+          margin: 0.5rem 0 0;
+          display: none;
+          color: brown;
+        }
+        .error.show {
+          display: block;
+        }
+      `}</style>
         </div>
     );
 }
