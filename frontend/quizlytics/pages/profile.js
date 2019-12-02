252add96d7c3aac520eab5280a200531d5ba798f
simmarum <mateusz0831@gmail.com>
2019-12-02 21:31:00 +0100

    Change profile page to class

--- frontend/quizlytics/pages/profile.js
+++ frontend/quizlytics/pages/profile.js
@@ -1,40 +1,46 @@
 import fetch from 'isomorphic-unfetch'
 import Layout from '../components/MyLayout'
 import nextCookie from 'next-cookies'
+import { Component } from 'react'
 import { auth, get_auth_header } from '../utils/auth'
 import { api_path } from '../utils/api_path'
 
-const Profile = props => {
-  const user_profile = props.results[0]
-  console.log("#@", user_profile)
-  return (
-    <div>
-      <h1>Profile</h1>
+class Profile extends Component {
 
-    </div>
-  )
-}
+  render() {
+    const user_profile = this.props.results[0]
+    console.log("#@", user_profile)
+    return (
+      <div>
+        <h1>Profile</h1>
 
-Profile.getInitialProps = async ctx => {
-  // We use `nextCookie` to get the cookie and pass the token to the
-  // frontend in the `props`.
-  const token = auth(ctx);
+      </div>
+    )
+  }
 
-  const url = api_path.users
-  try {
-    const response = await fetch(url, {
-      headers: get_auth_header(token)
-    })
-    console.log("$", response)
-    if (response.ok) {
-      return await response.json()
-    } else {
-      console.log(response)
+
+  static async getInitialProps(ctx) {
+    // We use `nextCookie` to get the cookie and pass the token to the
+    // frontend in the `props`.
+    const token = auth(ctx);
+
+    const url = api_path.users
+    try {
+      const response = await fetch(url, {
+        headers: get_auth_header(token)
+      })
+      console.log("$", response)
+      if (response.ok) {
+        return await response.json()
+      } else {
+        console.log("E1")
+        console.log(response)
+      }
+    } catch (error) {
+      // Implementation or Network error
+      console.log("E2")
+      console.log(error)
     }
-  } catch (error) {
-    // Implementation or Network error
-    console.log(error)
   }
 }
-
 export default Profile
