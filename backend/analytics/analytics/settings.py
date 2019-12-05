50a7d1affdfa89c48bbdabdb2060ef810746d894
simmarum <mateusz0831@gmail.com>
2019-12-05 20:00:52 +0100

    Shorter duration of auth token

--- backend/analytics/analytics/settings.py
+++ backend/analytics/analytics/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -135,5 +136,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=30)
 }
 AUTH_USER_MODEL = 'quiz.User'
