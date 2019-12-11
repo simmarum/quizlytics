3153aaaec4bbfc74045821f30253b4f9ae7ee88e
simmarum <mateusz0831@gmail.com>
2019-12-11 23:30:30 +0100

    Add information about how reverse url for testing

---
+++ backend/analytics/quiz/tests/__init__.py
@@ -0,0 +1,8 @@
+# How to reverse URL for testing
+# URL_Style           HTTP_Method     Action      URL_Name
+# {prefix}/           GET             list        {basename}-list
+#                     POST            create
+# {prefix}/{lookup}/  GET             retrieve    {basename}-detail
+#                     PUT             update
+#                     PATCH           partial_update
+#                     DELETE          destroy
