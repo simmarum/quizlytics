66be8371a8f55d2b293d0e28addec62759174280
simmarum <mateusz0831@gmail.com>
2019-12-12 23:52:44 +0100

    Add id for question in API to further retireve

--- backend/analytics/quiz/serializers.py
+++ backend/analytics/quiz/serializers.py
@@ -21,7 +21,7 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = User
-        fields = ('url', 'email', 'first_name', 'last_name', 'password', 'profile')
+        fields = ('url', 'id', 'email', 'first_name', 'last_name', 'password', 'profile')
         extra_kwargs = {
             'password': {'write_only': True},
             'first_name': {'required': True},
