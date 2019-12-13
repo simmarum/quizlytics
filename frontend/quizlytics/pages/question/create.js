3bc6808cb94bf116fb19fd44ecfa3092aa301669
simmarum <mateusz0831@gmail.com>
2019-12-13 22:21:03 +0100

    Move add_answer to utils

--- frontend/quizlytics/pages/question/create.js
+++ frontend/quizlytics/pages/question/create.js
@@ -1,5 +1,6 @@
 import { Component } from 'react'
 import { auth, get_auth_header } from '../../utils/auth'
+import { add_answer } from '../../utils/question'
 import { api_path, fetch_get, fetch_patch, fetch_post } from '../../utils/api_path'
 import Router from 'next/router'
 
@@ -12,6 +13,8 @@ class MyQuestionCreate extends Component {
       token: props.token,
       error: ''
     }
+
+    this.add_answer = add_answer
     this.handleChangeQTitle = this.handleChangeQTitle.bind(this)
     this.save_question = this.save_question.bind(this)
   }
@@ -51,50 +54,13 @@ class MyQuestionCreate extends Component {
     }
   }
 
-  add_answer() {
-    var q_answers = document.getElementById("q_answers");
-    var all_answers = document.getElementsByClassName("q_answer");
-    var new_id = 1;
-    var i = 0;
-    var j = all_answers.length
-    for (; i < j; i++) {
-      new_id = Math.max(parseInt(all_answers[i].getAttribute("id_int"), 10), new_id) + 1
-    }
-
-    var new_answer = document.createElement('div');
-    new_answer.setAttribute("class", "row q_answer");
-    new_answer.setAttribute("id_int", new_id);
-    let div_id = "a_" + new_id
-    new_answer.setAttribute("id", div_id);
-
-    var tb = document.createElement('button');
-    tb.setAttribute("class", "btn");
-    tb.setAttribute("data-div_id", div_id)
-    tb.addEventListener('click', function () {
-      var div_id_to_rm = this.getAttribute("data-div_id")
-      var div_to_rm = document.getElementById(div_id_to_rm)
-      div_to_rm.parentNode.removeChild(div_to_rm);
-    });
-    tb.innerHTML = "Remove"
-
-    var ti = document.createElement('input');
-    ti.setAttribute("class", "col-8 question");
-    ti.type = "text"
-    ti.id = `aa_${new_id}`
-    ti.innerHTML = `Answer ${new_id}`
-
-    new_answer.appendChild(tb)
-    new_answer.appendChild(ti)
-    q_answers.appendChild(new_answer)
-
-  }
   render() {
     return (
       <div>
         <div className='col-12 my_question'>
           <div className='row tt'>My Questions - create</div>
           <div className='row'>
-            <button className='btn offset-1' onClick={this.add_answer}>Add answer</button>
+            <button className='btn offset-1' onClick={() => this.add_answer(null)}>Add answer</button>
             <button className='btn offset-1' onClick={this.save_question}>Save question</button>
           </div>
           <div className="row align-items-center" id='q_title'>
---
+++ frontend/quizlytics/utils/question.js
@@ -0,0 +1,39 @@
+export function add_answer(answer_text) {
+  console.log(answer_text)
+  var q_answers = document.getElementById("q_answers");
+  var all_answers = document.getElementsByClassName("q_answer");
+  var new_id = 1;
+  var i = 0;
+  var j = all_answers.length
+  for (; i < j; i++) {
+    new_id = Math.max(parseInt(all_answers[i].getAttribute("id_int"), 10), new_id) + 1
+  }
+
+  var new_answer = document.createElement('div');
+  new_answer.setAttribute("class", "row q_answer");
+  new_answer.setAttribute("id_int", new_id);
+  let div_id = "a_" + new_id
+  new_answer.setAttribute("id", div_id);
+
+  var tb = document.createElement('button');
+  tb.setAttribute("class", "btn");
+  tb.setAttribute("data-div_id", div_id)
+  tb.addEventListener('click', function () {
+    var div_id_to_rm = this.getAttribute("data-div_id")
+    var div_to_rm = document.getElementById(div_id_to_rm)
+    div_to_rm.parentNode.removeChild(div_to_rm);
+  });
+  tb.innerHTML = "Remove"
+
+  var ti = document.createElement('input');
+  ti.setAttribute("class", "col-8 question");
+  ti.type = "text"
+  ti.id = `aa_${new_id}`
+  if (answer_text != null) {
+    ti.value = answer_text
+  }
+
+  new_answer.appendChild(tb)
+  new_answer.appendChild(ti)
+  q_answers.appendChild(new_answer)
+}
