1cdd2ecac1f46fc28d194ff7a8b0f5e984f5f037
simmarum <mateusz0831@gmail.com>
2019-12-14 20:58:23 +0100

    Add more models to admin django page

--- backend/analytics/quiz/admin.py
+++ backend/analytics/quiz/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from .models import User, UserProfile
+from .models import User, UserProfile, Question, QuestionAnswer, City, MailSend
 
 
 class UserProfileInline(admin.StackedInline):
@@ -29,3 +29,33 @@ class UserAdmin(BaseUserAdmin):
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email',)
     inlines = (UserProfileInline, )
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('uid', 'id', 'version', 'active', 'title')
+    search_fields = ('title', 'uid', 'id')
+    list_filter = ('active',)
+    ordering = ('uid',)
+
+
+@admin.register(QuestionAnswer)
+class QuestionAnswerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question_id', 'answer_number', 'answer_text')
+    search_fields = ('answer_text',)
+    list_filter = ('answer_number',)
+    ordering = ('question_id',)
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+@admin.register(MailSend)
+class MailSendAdmin(admin.ModelAdmin):
+    list_display = ('to_email', 'subject', 'message')
+    search_fields = ('subject', 'to_email')
+    ordering = ('to_email',)
